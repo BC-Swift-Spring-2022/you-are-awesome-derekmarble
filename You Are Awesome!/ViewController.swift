2fc35ad40a3729ac0ab6d9267bfc7c47abfcda6a
Derek Marble <dmarble20@gmail.com>
2022-01-30 14:28:44 -0500

    2.7: App layout, auto resizing

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("🦍 viewDidLoad has run")
+        messageLabel.text = ""
     }
 
 
