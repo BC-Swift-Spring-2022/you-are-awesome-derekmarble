8a33f3bd60ab20ad45f354f010992401d96c889f
Derek Marble <dmarble20@gmail.com>
2022-01-31 14:32:11 -0500

    2.10: Declared an Array

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        
-        print(imageNumber)
+        let messages = ["You are Awesome!","You Are Great!","You Are Fantastic!", "Fabulous? That's You!"]
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
 //        let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
@@ -32,9 +37,7 @@ class ViewController: UIViewController {
       
         
         
-        //        let awesomeMessage = "You Are Awesome!"
-        //        let greatMessage = "You Are Great!"
-        //        let bombMessage = "You Are Da Bomb!"
+
         //
         //
         //        if messageLabel.text == awesomeMessage {
